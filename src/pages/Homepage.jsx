78585455ea4511539ecac93fdce8c050660e53cc
moreira8125 <your_github_email>
2024-04-09 17:55:06 +0100

    font issues

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,7 +2,7 @@
 function Homepage() {
   return (
     <div>
-      <h1 className="text-4xl text-logo-blue text-center my-12 font-merriweather mx-2 font-semibold">
+      <h1 className="text-4xl text-logo-blue text-center my-12 italic mx-2 font-semibold">
         Hello! I'm <span className="text-logo-gold">Diogo</span>, welcome to my
         portfolio.
       </h1>
