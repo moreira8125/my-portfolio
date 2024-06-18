bbad0abe0b011b1273bf26b4d57a35a7b34f0c98
moreira8125 <your_github_email>
2024-06-18 16:40:29 +0100

    error fixed

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,14 +1,11 @@
-import { useEffect, useState } from "react";
-import projectsImport from "../db.json";
+import { useState } from "react";
+import projects from "../db.json";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 
 function Projects() {
   // Initialize showText as an array of booleans
-  const [showText, setShowText] = useState(
-    Array(projectsImport.length).fill(false)
-  );
-  const [projects, setProjects] = useState(projectsImport);
+  const [showText, setShowText] = useState(Array(projects.length).fill(false));
 
   // Function to toggle the text visibility for a specific project
   const toggleText = (index) => {
@@ -22,93 +19,86 @@ function Projects() {
 
   return (
     <div className="h-full">
-      {projects.length === 0 && (
-        <>
-          <h1>Loading</h1>
-        </>
-      )}
-      {projects.length > 0 && (
-        <>
-          <h1 className="text-4xl lg:text-6xl lg:mb-12 text-center text-logo-blue font-bold mt-12 tracking-tighter animate-slideinup">
-            Projects
-          </h1>
-          <div className="flex items-center flex-col justify-center gap-4 lg:gap-24 lg:flex-row lg:justify-around lg:items-start lg:flex-wrap">
-            {projects.map((oneProject, index) => (
-              <div
-                className="shadow-custom rounded-lg w-2/3 mt-8 bg-white shadow-logo-gold pb-4 lg:w-1/4"
-                key={oneProject.id}
+      <h1 className="text-4xl lg:text-6xl  lg:mb-12 text-center text-logo-blue font-bold mt-12 tracking-tighter animate-slideinup">
+        Projects
+      </h1>
+      <div className="flex items-center flex-col justify-center gap-4 lg:gap-24 lg:flex-row lg:justify-around lg:items-start lg:flex-wrap">
+        {projects.map((oneProject, index) => {
+          return (
+            <div
+              className="shadow-custom rounded-lg w-2/3 mt-8  bg-white shadow-logo-gold pb-4 lg:w-1/4"
+              key={oneProject.id}
+            >
+              <Link to={oneProject.link} target="_blank">
+                <img src={oneProject.image} alt="" className="rounded-t-lg" />
+              </Link>
+
+              <h1 className="text-center text-logo-blue text-2xl font-bold mt-2">
+                {oneProject.name}
+              </h1>
+              <h2 className="text-center text-logo-blue text-lg lg:text-xl font-semibold h-24 flex flex-col justify-center">
+                Technologies used: <br />
+                <span className="text-sm lg:text-base">
+                  {oneProject.technologies.join(", ")}
+                </span>
+              </h2>
+
+              <p
+                className={`p-2 text-sm lg:text-base transition-all duration-300 ease-in-out text-logo-blue ${
+                  !showText[index] ? "line-clamp-3 max-h-16" : "max-h-full"
+                }`}
               >
-                <Link to={oneProject.link} target="_blank">
-                  <img src={oneProject.image} alt="" className="rounded-t-lg" />
-                </Link>
+                {oneProject.description}
+              </p>
 
-                <h1 className="text-center text-logo-blue text-2xl font-bold mt-2">
-                  {oneProject.name}
-                </h1>
-                <h2 className="text-center text-logo-blue text-lg lg:text-xl font-semibold h-24 flex flex-col justify-center">
-                  Technologies used: <br />
-                  <span className="text-sm lg:text-base">
-                    {oneProject.technologies.join(", ")}
+              <button
+                className="border-2 bg-logo-blue text-white px-2 py-1 rounded-full flex items-center mx-auto mt-2"
+                onClick={() => toggleText(index)}
+              >
+                {showText[index] ? <p>Read Less</p> : <p>Read More</p>}
+                {!showText[index] ? (
+                  <span className="ml-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="2"
+                      stroke="currentColor"
+                      className="size-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                      />
+                    </svg>
                   </span>
-                </h2>
-
-                <p
-                  className={`p-2 text-sm lg:text-base transition-all duration-300 ease-in-out text-logo-blue ${
-                    !showText[index] ? "line-clamp-3 max-h-16" : "max-h-full"
-                  }`}
-                >
-                  {oneProject.description}
-                </p>
-
-                <button
-                  className="border-2 bg-logo-blue text-white px-2 py-1 rounded-full flex items-center mx-auto mt-2"
-                  onClick={() => toggleText(index)}
-                >
-                  {showText[index] ? <p>Read Less</p> : <p>Read More</p>}
-                  {!showText[index] ? (
-                    <span className="ml-1">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="2"
-                        stroke="currentColor"
-                        className="size-4"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="m19.5 8.25-7.5 7.5-7.5-7.5"
-                        />
-                      </svg>
-                    </span>
-                  ) : (
-                    <span className="ml-1">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="2"
-                        stroke="currentColor"
-                        className="size-4"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="m4.5 15.75 7.5-7.5 7.5 7.5"
-                        />
-                      </svg>
-                    </span>
-                  )}
-                </button>
-              </div>
-            ))}
-          </div>
-          <div className="mt-12">
-            <Footer />
-          </div>
-        </>
-      )}
+                ) : (
+                  <span className="ml-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="2"
+                      stroke="currentColor"
+                      className="size-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m4.5 15.75 7.5-7.5 7.5 7.5"
+                      />
+                    </svg>
+                  </span>
+                )}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+      <div className="mt-12">
+        <Footer> </Footer>
+      </div>
     </div>
   );
 }
